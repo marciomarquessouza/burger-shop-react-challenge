d4a682eb6f0482afe798ffe43c32b97a0056c777
marciomarquessouza <marciomultimedia@gmail.com>
2018-12-16 20:43:55 -0200

    Burger reduce

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,18 @@ import classes from './Burger.css';
 import BurgerIngredients from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
-  const impIngredients = Object.keys(props.ingredients).map((key) => {
+  let impIngredients = Object.keys(props.ingredients).map((key) => {
     return [...Array(props.ingredients[key])].map((_, index) => {
       return <BurgerIngredients key={ key + index } type={ key } />
     });
-  });
+  })
+  .reduce((arr, el) => {
+    return arr.concat(el);
+  }, []);
+  if (impIngredients.length === 0) {
+    impIngredients = <p>Please, start to add the ingredients</p>
+  }
+  console.log(impIngredients);
   return (
     <div className={ classes.Burger }>
       <BurgerIngredients type="bread-top"></BurgerIngredients>
