2aebc8eda388119405316fee7a860a07f5d83b65
marciomarquessouza <marciomultimedia@gmail.com>
2019-01-25 00:31:05 -0200

    Performance improvements

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,41 +1,45 @@
-import React from 'react';
-import Aux from '../../../hoc/Aux';
+import React, { Component } from 'react';
+import Aux from '../../../hoc/Aux/Aux';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-  const ingredients = props.ingredients;
-  const ingredientSummary = Object.keys(ingredients)
-    .map((ingredient) => {
-      return (
-        <li key={ ingredient }>
-          <span style={{textTransform: 'captalize'}}>{ ingredient }</span>:
-          { props.ingredients[ingredient]}
-        </li>
-      );
-  })
-  return (
-    <Aux>
-      <h3>Your order</h3>
-      <p>A delicious burger with the following ingredients:</p>
-      <ul>
-        { ingredientSummary }
-      </ul>
-      <p><strong>Total Price:</strong> { props.price } </p>
-      <p>Continue to Checkout?</p>
-      <Button
-        btnType = "Danger"
-        clicked = { props.purchaseCanceled }
-      >
-        Cancel
-      </Button>
-      <Button
-        btnType = "Success"
-        clicked = { props.purchaseContinue }
-      >
-        Go!
-      </Button>
-    </Aux>
-  )
+class OrderSummary extends Component {
+  // Could be a functional component, only to create a example to map when the modal is called
+  render () {
+    const ingredients = this.props.ingredients;
+    const ingredientSummary = Object.keys(ingredients)
+      .map((ingredient) => {
+        return (
+          <li key={ ingredient }>
+            <span style={{textTransform: 'captalize'}}>{ ingredient }</span>:
+            { this.props.ingredients[ingredient]}
+          </li>
+        );
+    })
+    return (
+      <Aux>
+        <h3>Your order</h3>
+        <p>A delicious burger with the following ingredients:</p>
+        <ul>
+          { ingredientSummary }
+        </ul>
+        <p><strong>Total Price:</strong> { this.props.price } </p>
+        <p>Continue to Checkout?</p>
+        <Button
+          btnType = "Danger"
+          clicked = { this.props.purchaseCanceled }
+        >
+          Cancel
+        </Button>
+        <Button
+          btnType = "Success"
+          clicked = { this.props.purchaseContinue }
+        >
+          Go!
+        </Button>
+      </Aux>
+    )
+  }
+
 };
 
-export default orderSummary;
+export default OrderSummary;
